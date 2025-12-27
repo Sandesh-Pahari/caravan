cc17c68d50a2c99e916561c206f71a1938bb41ea
Sandesh Pahari <sandeshpahari05@gmail.com>
2025-12-27 09:23:30 +0545

    services.php updated

--- config/services.php
+++ config/services.php
@@ -36,8 +36,8 @@ return [
     ],
 
     'stripe' => [
-        'secret' => env('STRIPE_SECRET_KEY'),
-        'public_key' => env('STRIPE_PUBLIC_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+        'public_key' => env('STRIPE_KEY'),
     ],
 
     'khalti' => [
