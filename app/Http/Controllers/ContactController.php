723389e0ddc26b299c15e0209734554bc75653f7
Sandesh Pahari <sandeshpahari05@gmail.com>
2026-01-22 07:19:06 +0545

    Save contact form submissions to database before queuing mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreContactRequest;
 use App\Mail\ContactMail;
+use App\Models\ContactMessage;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Mail;
 
@@ -11,6 +12,13 @@ class ContactController extends Controller
 {
     public function send(StoreContactRequest $request): JsonResponse
     {
+        ContactMessage::query()->create([
+            'name' => $request->validated('name'),
+            'email' => $request->validated('email'),
+            'phone' => $request->validated('phone'),
+            'message' => $request->validated('message'),
+        ]);
+
         Mail::to(config('mail.from.address'))
             ->send(new ContactMail(
                 senderName: $request->validated('name'),
